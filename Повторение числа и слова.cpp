476bee8ec5c9ec7eb31e89bbf2d470255cc85bd4
DenisBlackKnight <DBatyrkaev2@Mail.ru>
2025-09-17 20:17:12 +0300

    Add files via upload

---
+++ Повторение числа и слова.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int number{ 0 };
+	std::string word{0};
+
+	std::cout << "Введите число" << std::endl;
+	std::cin >> number;
+
+	std::cout << "Введите слово или слова" << std::endl;
+	std::getline(std::cin >> std::ws,word);
+
+	std::cout << "Число: " << number << " | Слово или слова: " << word << std::endl;
+}
