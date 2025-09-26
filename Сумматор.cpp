f83b276dcdf7316aee1e194b0711d0dea3ad2c16
DenisBlackKnight <DBatyrkaev2@Mail.ru>
2025-09-26 21:44:21 +0300

    Add files via upload

---
+++ Сумматор.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main() 
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int value{};
+	int sum{};
+
+	do
+	{
+		std::cout << "Введите целое число или число '0' чтобы закончить: ";//<< std::endl;
+		std::cin >> value;
+		sum += value;
+	} while (value != 0);
+	std::cout << "Сумма: " << sum;
+}
