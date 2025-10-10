9e72b79f6027af2ca2101ba9862fa936fce18407
DenisBlackKnight <DBatyrkaev2@Mail.ru>
2025-10-10 20:30:20 +0300

    Add files via upload

---
+++ МПИХД1Адреса переменных.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+
+
+int main()
+{
+	int a         { 0 };
+	short b       { 0 };
+	long c        { 0 };
+	long long d   { 0 };
+	float e       { 0 };
+	double f      { 0 };
+	long double g { 0 };
+	bool h        { 0 };
+
+
+	setlocale(LC_ALL, "rus");
+	std::cout << "short: " << '\t' << '\t' << &b << ' ' << sizeof(b) << std::endl;
+	std::cout << "int: " << '\t' << '\t' << &a << ' ' << sizeof(a) << std::endl;
+	std::cout << "long: " << '\t' << '\t' << &c << ' ' <<  sizeof(c) << std::endl;
+	std::cout << "long long: " << '\t' << &d << ' ' <<  sizeof(d) << std::endl;
+	std::cout << "float: " << '\t' << '\t' << &e << ' ' <<  sizeof(e) << std::endl;
+	std::cout << "double: " << '\t' << &f << ' ' <<  sizeof(f) << std::endl;
+	std::cout << "long double: " << '\t' << &g << ' ' <<  sizeof(g) << std::endl;
+	std::cout << "bool: " << '\t' << '\t' << &h << ' ' <<  sizeof(h);
+}
---
+++ МПИХД2Обмен значениями.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
+
+void swap (int& a, int& b)
+{
+	a = a - b;
+	b = a + b;
+	a = b - a;
+}
+
+
+
+int main(int argc, char** argv)
+{
+	int a = 5, b = 8;
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	swap(a, b);
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	return 0;
+}
