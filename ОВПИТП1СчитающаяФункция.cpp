fd95568bea95d441de231da944ff4319c1506b3c
DenisBlackKnight <DBatyrkaev2@Mail.ru>
2025-10-13 19:08:23 +0300

    Add files via upload

---
+++ ОВПИТП1СчитающаяФункция.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+
+void counting_function() {
+    static int a{0};
+    a++;
+    std::cout << "Количество вызовов функции counting_function(): " << a << std::endl;
+
+}
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "rus");
+    for (int i = 0; i < 15; i++)
+    {
+        counting_function();
+    }
+}
---
+++ ОВПИТП2ОдинаковыеНоРазныеФункции.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+
+namespace arithmetic {
+
+    namespace sum 
+    {
+        void calc(int x, int y) {
+            std::cout << "Сложение: " << x + y << std::endl;
+        }
+    }
+
+    namespace diff 
+    {
+        void calc(int x, int y) {
+            std::cout << "Вычитание: " << x - y << std::endl;
+        }
+    }
+
+    namespace multiplication 
+    {
+        void calc(int x, int y) {
+            std::cout << "Умножение: " << x * y << std::endl;
+        }
+    }
+
+    namespace division 
+    {
+        void calc(int x, int y) {
+            std::cout << "Деление: " << x / y << std::endl;
+        }
+    }
+}
+
+int main(int argc, char** argv)
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int x = 6, y = 9;
+    std::cout << "x = " << x << ", y = " << y << std::endl;
+    arithmetic::sum::calc(x,y);
+    arithmetic::diff::calc(x, y);
+    arithmetic::multiplication::calc(x, y);
+    arithmetic::division::calc(x, y);
+
+}
