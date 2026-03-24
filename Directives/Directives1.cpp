d22b5e3221bf617c535dbf7c968f326c3711f507
DenisBlackKnight <DBatyrkaev2@Mail.ru>
2026-03-24 16:10:10 +0300

    Add files via upload

---
+++ Directives/Directives1.cpp
@@ -0,0 +1,35 @@
+﻿#include  <iostream>
+
+#define MODE 1
+
+#ifndef MODE
+#error cannot find the MODE
+#endif
+
+#if MODE == 1
+int add(int num1, int num2);
+#endif
+
+int main() 
+{
+#if MODE == 0
+	std::cout << "Training mode\n";
+#elif MODE == 1
+	std::cout << "Combat mode\n";
+	int a{}, b{};
+	std::cout << "Enter first number: ";
+	std::cin >> a;
+	std::cout << "Enter second number: ";
+	std::cin >> b;
+	std::cout << "Result = " << add(a, b);
+#else
+	std::cout << "Unknown mode\n";
+#endif
+}
+
+#if MODE == 1
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
+#endif
